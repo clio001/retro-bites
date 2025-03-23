86c7edcd747de25464a10eb83bf50ed7cabc52af
clio001 <johnwoitkowitz@hotmail.com>
2025-03-23 11:51:43 +0100

    added markers to map, added anchor link to map for Eingang

--- static/js/map.js
+++ static/js/map.js
@@ -14,3 +14,37 @@ L.tileLayer("https://tile.openstreetmap.de/{z}/{x}/{y}.png", {
   maxZoom: 50,
   attribution: "Tiles by Openstreetmap",
 }).addTo(map);
+
+
+const testLocations = [
+  {
+    name: "Test 1",
+    geoReferences: {
+      lat: 52.516389,
+      long: 13.377778
+    },
+  },
+  {
+    name: "Test 2",
+    geoReferences: {
+      lat: 52.526389,
+      long: 13.377778
+    },
+  }
+]
+
+
+
+const setMarkers = (testLocations) => {
+  testLocations.map((element) => {
+    let marker = L.marker([element.geoReferences.lat, element.geoReferences.long]).addTo(map);
+
+    const popupCard = `<b>${element.name}</b><p>Latitude: ${element.geoReferences.lat}<br>Longitude: ${element.geoReferences.long}</p>`
+
+    marker.bindPopup(popupCard)
+  });
+};
+
+
+setMarkers(testLocations);
+
