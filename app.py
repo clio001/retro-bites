adac6b187180e53829dc7657ee7436e334499c5c
Roman Kuhn <roman.k@gmx.com>
2025-04-01 22:33:40 +0200

    add api endpoints for locations and items -- because why not?

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template, redirect, url_for, abort
+from flask import Flask, request, render_template, redirect, url_for, abort, jsonify
 import json
+
 
 app = Flask(__name__)
 app.config['FREEZER_BASE_URL'] = 'https://r0man-ist.github.io/retro-bites/'
@@ -34,6 +35,41 @@ def item_nr(item_id):
 
         
     return render_template("item.html", item=item,  _external=True)
+
+# api endpoint to get item details by ID
+@app.route('/api/item/<int:item_id>/')
+def item_api(item_id):
+    with open("static/db.json", "r", encoding="utf-8") as f:
+        COLLECTION = json.load(f)
+
+    # Find the item with the given ID
+    item = next((item for item in COLLECTION if item["id"] == item_id), None)
+    
+    if item is None:
+        return jsonify({"error": "Item not found"}), 404
+    
+    return jsonify(item)
+    
+
+@app.route('/api/locations/')
+def locations():
+    with open("static/db.json", "r", encoding="utf-8") as f:
+       COLLECTION = json.load(f)
+    # Extract unique locations from the collection
+    # Assuming each menu has a 'place' key with 'lat', 'long', and 'Name'
+    # Create a dictionary to store unique locations
+    unique_locations = {}
+    for item in COLLECTION:
+        place = item.get('place')
+        if place:
+            lat = place.get('lat')
+            long = place.get('long')
+            name = place.get('Name')
+            if lat and long and name:
+                unique_locations[name] = {'lat': lat, 'long': long}
+    # Return the unique locations as JSON
+    return jsonify(unique_locations)
+    print(unique_locations)
 
 @app.route("/sammlung/")
 def sammlung():
