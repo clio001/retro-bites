4678aecacada01157f23cf2527a2b28f32c76277
clio001 <johnwoitkowitz@hotmail.com>
2025-04-02 18:54:50 +0200

    add db fetch for markers in map.js

--- static/js/map.js
+++ static/js/map.js
@@ -19,40 +19,30 @@ const myIcon = L.icon({
     iconUrl: 'static/marker-icon.svg',
     iconSize: [48, 48],
     iconAnchor: [0, 0],
-    popupAnchor: [-3, -76]
+    popupAnchor: [-3, -76] 
 });
 
+const fetch_db = async () => {
+  const response = await fetch("static/db.json");
+  const result = await response.json()
+  return result
+}
 
-const testLocations = [
-  {
-    id: 4,
-    name: "Brandenburger Tor",
-    geoReferences: {
-      lat: 52.516389,
-      long: 13.377778
-    },
-  },
-  {
-    id: 3,
-    name: "Stadtschloss ",
-    geoReferences: {
-      lat: 52.516389,
-      long: 13.397778
-    },
-  }
-]
+const get_locations = async () => {
+  const locations = await fetch_db();
+  console.log(locations)
+  setMarkers(locations);
+}
 
-
-
-const setMarkers = (testLocations) => {
-  testLocations.map((element) => {
-    let marker = L.marker([element.geoReferences.lat, element.geoReferences.long], { keyboard: true, icon: myIcon, }).addTo(map);
+const setMarkers = (locations) => {
+  locations.map((element) => {
+    let marker = L.marker([element.place.lat, element.place.long], { keyboard: true, icon: myIcon, }).addTo(map);
 
     const popupCard = `
     <div class="card" style="width: 22rem; border: none;">
       <div class="card-body">
         <img src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Bundesarchiv_Bild_183-R99514%2C_Berlin%2C_Wilhelmplatz%2C_Hotel_%22Kaiserhof%22.jpg" style="width: 20rem;" alt="...">
-        <h5 class="mt-3">${element.name}</h5>
+        <h5 class="mt-3">${element.place.Name}</h5>
         <p>Das Hotel Kaiserhof war das erste Luxushotel in Berlin. Es stand am Wilhelmplatz 3–5 schräg gegenüber der Reichskanzlei im damaligen Berliner Regierungsviertel. Das Hotel wurde im Oktober 1875 eröffnet und am 23. November 1943 durch mehrere Bombeneinschläge zerstört.</p>
         <div class="small text-body-secondary text-center mt-4 mb-2">Quelle: Wikidata und Wikipedia</div>
         <hr/>
@@ -66,9 +56,6 @@ const setMarkers = (testLocations) => {
     marker.bindPopup(popupCard, {maxWidth: 800});
   });
 };
-
-
-setMarkers(testLocations);
 
 const backButton = L.control({ position: "topright" });
 
